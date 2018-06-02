3f8d5a5b3283f7bdef5f3382872c3dd6c0408d4a
KajalSandilya <kajalsandilya.ks.ks@gmail.com>
2018-06-02 17:50:18 +0530

    Adding First Model

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace MovieProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MovieProjectDbContext>(options => options.UseSqlServer(".."));
+            services.AddDbContext<MovieProjectDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc();
         }
 
